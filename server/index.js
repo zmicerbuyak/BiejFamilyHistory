4a2f5e3d650565e03c5895b019a496f3dfc1d08a
zmicer <zmicerbuyak@gmail.com>
2025-05-20 21:05:42 +0200

    Updates

---
+++ server/index.js
@@ -0,0 +1,181 @@
+import { jsx, jsxs } from "react/jsx-runtime";
+import { PassThrough } from "node:stream";
+import { createReadableStreamFromReadable } from "@react-router/node";
+import { ServerRouter, useMatches, useActionData, useLoaderData, useParams, useRouteError, Meta, Links, ScrollRestoration, Scripts, Outlet, isRouteErrorResponse } from "react-router";
+import { isbot } from "isbot";
+import { renderToPipeableStream } from "react-dom/server";
+import { createElement } from "react";
+const streamTimeout = 5e3;
+function handleRequest(request, responseStatusCode, responseHeaders, routerContext, loadContext) {
+  return new Promise((resolve, reject) => {
+    let shellRendered = false;
+    let userAgent = request.headers.get("user-agent");
+    let readyOption = userAgent && isbot(userAgent) || routerContext.isSpaMode ? "onAllReady" : "onShellReady";
+    const { pipe, abort } = renderToPipeableStream(
+      /* @__PURE__ */ jsx(ServerRouter, { context: routerContext, url: request.url }),
+      {
+        [readyOption]() {
+          shellRendered = true;
+          const body = new PassThrough();
+          const stream = createReadableStreamFromReadable(body);
+          responseHeaders.set("Content-Type", "text/html");
+          resolve(
+            new Response(stream, {
+              headers: responseHeaders,
+              status: responseStatusCode
+            })
+          );
+          pipe(body);
+        },
+        onShellError(error) {
+          reject(error);
+        },
+        onError(error) {
+          responseStatusCode = 500;
+          if (shellRendered) {
+            console.error(error);
+          }
+        }
+      }
+    );
+    setTimeout(abort, streamTimeout + 1e3);
+  });
+}
+const entryServer = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  default: handleRequest,
+  streamTimeout
+}, Symbol.toStringTag, { value: "Module" }));
+function withComponentProps(Component) {
+  return function Wrapped() {
+    const props = {
+      params: useParams(),
+      loaderData: useLoaderData(),
+      actionData: useActionData(),
+      matches: useMatches()
+    };
+    return createElement(Component, props);
+  };
+}
+function withErrorBoundaryProps(ErrorBoundary3) {
+  return function Wrapped() {
+    const props = {
+      params: useParams(),
+      loaderData: useLoaderData(),
+      actionData: useActionData(),
+      error: useRouteError()
+    };
+    return createElement(ErrorBoundary3, props);
+  };
+}
+function Layout({
+  children
+}) {
+  return /* @__PURE__ */ jsxs("html", {
+    lang: "en",
+    children: [/* @__PURE__ */ jsxs("head", {
+      children: [/* @__PURE__ */ jsx("meta", {
+        charSet: "utf-8"
+      }), /* @__PURE__ */ jsx("meta", {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
+      }), /* @__PURE__ */ jsx(Meta, {}), /* @__PURE__ */ jsx(Links, {})]
+    }), /* @__PURE__ */ jsxs("body", {
+      children: [children, /* @__PURE__ */ jsx(ScrollRestoration, {}), /* @__PURE__ */ jsx(Scripts, {})]
+    })]
+  });
+}
+const root = withComponentProps(function App() {
+  return /* @__PURE__ */ jsx(Outlet, {});
+});
+const ErrorBoundary = withErrorBoundaryProps(function ErrorBoundary2({
+  error
+}) {
+  let message = "Oops!";
+  let details = "An unexpected error occurred.";
+  let stack;
+  if (isRouteErrorResponse(error)) {
+    message = error.status === 404 ? "404" : "Error";
+    details = error.status === 404 ? "The requested page could not be found." : error.statusText || details;
+  }
+  return /* @__PURE__ */ jsxs("main", {
+    className: "pt-16 p-4 container mx-auto",
+    children: [/* @__PURE__ */ jsx("h1", {
+      children: message
+    }), /* @__PURE__ */ jsx("p", {
+      children: details
+    }), stack]
+  });
+});
+const route0 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  ErrorBoundary,
+  Layout,
+  default: root
+}, Symbol.toStringTag, { value: "Module" }));
+function loader() {
+  return {
+    name: "React Router"
+  };
+}
+const home = withComponentProps(function Home({
+  loaderData
+}) {
+  return /* @__PURE__ */ jsxs("div", {
+    className: "text-center p-4",
+    children: [/* @__PURE__ */ jsxs("h1", {
+      className: "text-2xl",
+      children: ["Hello, ", loaderData.name]
+    }), /* @__PURE__ */ jsx("a", {
+      className: "block mt-2 text-blue-500 underline hover:text-blue-600",
+      href: "https://reactrouter.com/docs",
+      children: "React Router Docs"
+    })]
+  });
+});
+const route1 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  default: home,
+  loader
+}, Symbol.toStringTag, { value: "Module" }));
+const serverManifest = { "entry": { "module": "/assets/entry.client-SQZkSKxz.js", "imports": ["/assets/chunk-D4RADZKF-BXVSF3ox.js"], "css": [] }, "routes": { "root": { "id": "root", "parentId": void 0, "path": "", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": true, "module": "/assets/root-BRbO9jDv.js", "imports": ["/assets/chunk-D4RADZKF-BXVSF3ox.js", "/assets/with-props-BMOtgW5C.js"], "css": ["/assets/root-C0kErg8T.css"], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 }, "routes/home": { "id": "routes/home", "parentId": "root", "path": void 0, "index": true, "caseSensitive": void 0, "hasAction": false, "hasLoader": true, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": false, "module": "/assets/home-Ci8AMsgv.js", "imports": ["/assets/with-props-BMOtgW5C.js", "/assets/chunk-D4RADZKF-BXVSF3ox.js"], "css": [], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 } }, "url": "/assets/manifest-af8c7bbe.js", "version": "af8c7bbe", "sri": void 0 };
+const assetsBuildDirectory = "build\\client";
+const basename = "/";
+const future = { "unstable_middleware": false, "unstable_optimizeDeps": false, "unstable_splitRouteModules": false, "unstable_subResourceIntegrity": false, "unstable_viteEnvironmentApi": false };
+const ssr = true;
+const isSpaMode = false;
+const prerender = [];
+const routeDiscovery = { "mode": "lazy", "manifestPath": "/__manifest" };
+const publicPath = "/";
+const entry = { module: entryServer };
+const routes = {
+  "root": {
+    id: "root",
+    parentId: void 0,
+    path: "",
+    index: void 0,
+    caseSensitive: void 0,
+    module: route0
+  },
+  "routes/home": {
+    id: "routes/home",
+    parentId: "root",
+    path: void 0,
+    index: true,
+    caseSensitive: void 0,
+    module: route1
+  }
+};
+export {
+  serverManifest as assets,
+  assetsBuildDirectory,
+  basename,
+  entry,
+  future,
+  isSpaMode,
+  prerender,
+  publicPath,
+  routeDiscovery,
+  routes,
+  ssr
+};
